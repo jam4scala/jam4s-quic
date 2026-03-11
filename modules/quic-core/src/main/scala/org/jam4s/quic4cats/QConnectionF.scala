6fce1bf3c3aba72d91ac1def200c1643657bb6c4
sergey-astapov <serg.astapov@gmail.com>
2026-03-11 22:01:39 +0800

    - fix 'readAllBytes() blocks forever without timeout'

--- modules/quic-core/src/main/scala/org/jam4s/quic4cats/QConnectionF.scala
+++ modules/quic-core/src/main/scala/org/jam4s/quic4cats/QConnectionF.scala
@@ -1,15 +1,15 @@
 package org.jam4s.quic4cats
 
-import cats.effect.kernel.Sync
+import cats.effect.kernel.Async
 import net.luminis.quic.QuicConnection
 
 trait QConnection[F[_]]:
   def stream(bidirectional: Boolean = true): F[QStream[F]]
 
-final class QConnectionF[F[_]: Sync] private[quic4cats] (val underlying: QuicConnection)
+final class QConnectionF[F[_]: Async] private[quic4cats] (val underlying: QuicConnection)
     extends QConnection[F]:
 
   def stream(bidirectional: Boolean = true): F[QStream[F]] =
-    Sync[F].blocking {
+    Async[F].blocking {
       QStreamF[F](underlying.createStream(bidirectional))
     }
--- modules/quic-core/src/main/scala/org/jam4s/quic4cats/QStreamF.scala
+++ modules/quic-core/src/main/scala/org/jam4s/quic4cats/QStreamF.scala
@@ -1,26 +1,38 @@
 package org.jam4s.quic4cats
 
-import cats.effect.kernel.Sync
+import java.util.concurrent.TimeoutException
+
+import scala.concurrent.duration.FiniteDuration
+
+import cats.effect.kernel.Async
 import net.luminis.quic.QuicStream
 
 trait QStream[F[_]]:
   def read: F[Array[Byte]]
+  def readTimeout(timeout: FiniteDuration): F[Array[Byte]]
   def write(bytes: Array[Byte]): F[Unit]
   def closeOutput: F[Unit]
 
-final class QStreamF[F[_]: Sync](underlying: QuicStream) extends QStream[F]:
+final class QStreamF[F[_]: Async](underlying: QuicStream) extends QStream[F]:
 
   def read: F[Array[Byte]] =
-    Sync[F].blocking {
+    Async[F].blocking {
       underlying.getInputStream.readAllBytes()
     }
 
+  def readTimeout(timeout: FiniteDuration): F[Array[Byte]] =
+    Async[F].timeoutTo(
+      read,
+      timeout,
+      Async[F].raiseError(new TimeoutException(s"Read timed out after $timeout"))
+    )
+
   def write(bytes: Array[Byte]): F[Unit] =
-    Sync[F].blocking {
+    Async[F].blocking {
       underlying.getOutputStream.write(bytes)
     }
 
   def closeOutput: F[Unit] =
-    Sync[F].blocking {
+    Async[F].blocking {
       underlying.getOutputStream.close()
     }
